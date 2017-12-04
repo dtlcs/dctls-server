47e4ce226cee9556e67f597bb919ae7a0192ac73
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-12-04 15:14:48 +0530

    API get all users test

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+app.get("/api/users", function(req, res) {
+    connection.connect();
+
+    connection.query('SELECT * FROM user', function(err, rows, fields) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(rows);
+        }
+    });
+
+    connection.end();
+});
