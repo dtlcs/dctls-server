6e6b8895d70d8fe1f5fc4c8e2800fd8dae52c1e8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-12-07 10:48:21 +0530

    Parameter added to junction list function

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.post("/api/user/authenticate", function (req, res) {
 });
 
 // Get user junction list
-app.get("/api/user/junctions", function (req, res) {
+app.get("/api/user/junctions/:userId", function (req, res) {
     console.log(req.body);
     if (!req.body.userId) {
         handleError(res, "Invalid user input", "Must provide a user ID.", 400);
