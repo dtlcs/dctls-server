494e2e9935829167a3768b668ab543bc5dfc4873
Oshan Mudannayake <oshan.ivantha@gmail.com>
2017-12-11 21:06:02 +0530

    Access-Control-Allow-Origin * added.

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ var app = express();
 app.use(bodyParser.json());
 app.use('/pi', mqttRouter);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8080, function() {
     var port = server.address().port;
@@ -24,12 +31,12 @@ var server = app.listen(process.env.PORT || 8080, function() {
 });
 
 // Generic error handler used by all endpoints.
-function handleError(res, reason, message, code, err) {
+function handleError(res, reason, message, code) {
     console.log("ERROR: " + reason);
     res.status(code || 500).json({
         "error": message
     });
-    throw err;
+    // throw err;
 }
 
 // Get all users
