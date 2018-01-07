107b9260fbcd20befc0e2c323a91320fd4e7f761
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-01-07 11:42:09 +0530

    app.yaml added.

--- database/mysql-connection.js
+++ database/mysql-connection.js
@@ -1,19 +1,11 @@
 var mysql = require('mysql');
 
-// var connection = mysql.createPool({
-//   host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   user: "ae8ptu9hakhbthtj",
-//   password: "jy5105xcnjkesh5i",
-//   dateStrings: true,
-//   database: "j95ambgc4f7zrfai"
-// });
-
 var connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
+  host: "35.226.99.182",
+  user: "ivantha",
+  password: "cat",
   dateStrings: true,
-  database: "j95ambgc4f7zrfai"
+  database: "stls_db"
 });
 
 module.exports = connection;
