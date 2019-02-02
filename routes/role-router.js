5def72502e89bf6ad93a158f77a2f23413536d0f
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-02-03 01:34:37 +0530

    Apply minor fixes

--- routes/role-router.js
+++ routes/role-router.js
@@ -4,13 +4,13 @@ var connection = require('../database/mysql-connection');
 
 // Get all roles
 roleRouter.get("/all", function (req, res, next) {
-  connection.query('SELECT * FROM role', function (err, rows, fields) {
-    if (err) {
-      next(err);
-    } else {
-      res.status(200).json(rows);
-    }
-  });
+    connection.query('SELECT * FROM role', function (err, rows, fields) {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json(rows);
+        }
+    });
 });
 
 module.exports = roleRouter;
